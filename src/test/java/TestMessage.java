e99df543209f1333c5ac8e70d48652b1c59e8d10
Ariel <ariel.card@gmail.com>
2017-04-27 13:52:51 +0200

    Adding de first files

---
+++ src/test/java/TestMessage.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class TestMessage {
+	
+	
+	@Test
+	public void testMessage(){
+		String m = "hola";
+		assertEquals("hola",m);
+	}
+
+}
